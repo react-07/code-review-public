a6766b4a371ceb6ef0ca7920be5ec9f3eb4b6727
test user <coderunknown07@gmail.com>
2025-04-24 16:49:30 +0530

    Create buggy_inventory.py

---
+++ buggy_inventory.py
@@ -0,0 +1,25 @@
+class Product
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def discount(self, percent)
+        new_price = self.price * percent / 100
+        print("Discounted price is", newprice) 
+
+def main():
+    item = Product("Laptop", 1000)
+    item.discount(10)
+
+    items = ["Phone", "Tablet", "Monitor"]
+    for i in range(0, len(items)):
+        print(items[i+1]) 
+
+    value = int(input("Enter a number: "))
+    if value = 10:
+        print("Value is ten")
+    else
+        print("Not ten")
+
+if __name__ = "__main__": 
+    main()
